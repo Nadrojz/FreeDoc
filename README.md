ebbe4e4f4952d43ee14f821bec35c7ed8b4fc531
Nadrojz <jor.perrin@gmail.com>
2019-08-01 21:24:38 +0200

    readme & comments

--- README.md
+++ README.md
@@ -1,24 +1,29 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
 
-Things you may want to cover:
+Ce programme va générer grâce à la gem 'faker' des docteurs et des patients puis leur assigner une date et une ville de rendez vous.
+Pour générer des docteurs, patients et rendez vous, lancer la commande rails db:seed
+Cela permettra de remplir les tables de la BDD avec les informations contenues dans le fichier db/seeds.rb
+Il est conseillé d'utiliser la gem 'table_print' pour afficher les tables dans la console rails avec la commande "tp Nomdelatable.all"
 
-* Ruby version
 
-* System dependencies
+La table doctors est composée des colonnes suivantes :
+* ID
+* first_name
+* last_name
+* zip_code
+* specialty
+et chaque objet de la table sera lié à la table cities via la foreign key city_id
 
-* Configuration
 
-* Database creation
+La table patients est composée des colonnes suivantes :
+* ID
+* first_name
+* last_name
+et chaque objet de la table sera lié à la table cities via la foreign key city_id
 
-* Database initialization
 
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+La table appointments est composée des colonnes suivantes :
+* ID
+* Date
+et chaque objet de la table sera relié, via des foreign key, aux tables doctors, patients et cities
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require 'faker'
-
+# Supprime toutes les villes existantes et crée aléatoirement via la gem faker 10 noms de villes
 City.destroy_all
 10.times do
   City.create!(
@@ -15,29 +7,46 @@ City.destroy_all
     )
 end
 
+# Supprime tous les docteurs
 Doctor.destroy_all
+# Supprime tous les patients
 Patient.destroy_all
+#Va faire 50 fois les actions ci dessous
 50.times do |index|
+  # Créer un docteur
   d = Doctor.create
+  #Donner un prénom au docteur
   d.first_name = Faker::Name.first_name
+  #Donner un nom au docteur
   d.last_name = Faker::Name.last_name
+  #Assigne au hasard au docteur une des spécialités contenues dans l'array
   d.specialty = (["Allergy", "Anesthesiology", "Dermatology", "Radiology", "Emergency medecine", "Generalist", "Internal medecine", "Neurology", "Nuclear medecine", "Gynecology", "Ophtalmology", "Pathology", "Pediatrics", "Physical medecine", "Preventive medecine", "Psychiatry", "Radiation oncology", "Surgery", "Urology"]).sample
+  #Donner un zip_code
   d.zip_code = Faker::Address.zip_code
+  #Donner au hasard une des villes
   d.city = City.order('RANDOM()').first
+  #Sauvegarder les informations du docteur crée
   d.save
+  #Créer un patient et lui donne un prénom, nom, ville et sauvegarde
   p = Patient.create
   p.first_name = Faker::Name.first_name
   p.last_name = Faker::Name.last_name
   p.city = City.order('RANDOM()').first
   p.save
-
 end
 
+#Supprime tous les rendez vous
 Appointment.destroy_all
+#Va faire 100 fois les actions ci dessous
 100.times do
+  #Créer un rendez vous
   Appointment.create!(
+    #Lui assigner une date au hasard, à partir de maintenant jusqu'à 1 an dans le futur dans des horaires de journée et converti le string obtenu au format datetime
     date: Faker::Time.forward(days: 365, period: :day).to_datetime,
+    #Assigne aléatoirement un docteur au rendez vous
     doctor: Doctor.order('RANDOM()').first,
+    #Assigne aléatoirement un patient au rendez vous
     patient: Patient.order('RANDOM()').first,
+    #Assigne aléatoirement une ville au rendez vous
     city: City.order('RANDOM()').first)
 end
